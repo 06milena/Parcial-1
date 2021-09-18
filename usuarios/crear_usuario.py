c78f8a2bfceb0e21410e2a1b6c3d35f28007ef22
jeison iglesias <jeisonmavisoy2020@itp.edu.co>
2021-09-17 22:51:48 -0500

    'modify'

--- usuarios/crear_usuario.py
+++ usuarios/crear_usuario.py
@@ -1,4 +1,5 @@
 from config.db import DB
+import re
 
 
 def crearUsuario(nombre, email, contrasena):
@@ -18,7 +19,19 @@ def crearUsuario(nombre, email, contrasena):
 
 def crear():
       nombre= input('Ingrese el nombre: ')
-      email = input('Ingrese el correo: ')
+      email = input('Ingrese el correo: ') 
       contrasena = input('Ingrese la contraseña: ')
       crearUsuario(nombre,email,contrasena)
       bandera=1
+
+def validar_contraseña():
+      contrasena = input('Ingrese la contraseña: ')
+      while True:
+        if len(contrasena)<8:
+              print('contraseña de 8 caracteres')
+        elif re.search('[0-9]',contrasena) is None:
+              print('que tenga un numero')
+        elif re.search('[A-Z]',contrasena) is None:
+              print('que tenga una mayuscula')
+        else:
+              print('es correcto')
